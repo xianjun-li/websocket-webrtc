254ac6581c840e4ed719f63969634b84ae8e7bb0
lewis_dev li <lewis_dev@163.com>
2019-07-05 19:38:11 +0800

    samples

---
+++ samples/server/index.js
@@ -0,0 +1,34 @@
+const EventEmitter = require('events')
+
+const eventEmitter = new EventEmitter();
+
+const Koa = require('koa')
+
+const WebSocket = require('ws');
+
+const koaApp = new Koa()
+
+let server = koaApp.listen(3000)
+
+koaApp.use(async ctx => {
+    ctx.body = 'hello world'
+})
+
+let wsServer = new WebSocket.Server({ server })
+
+wsServer.on('connection', ws => {
+    eventEmitter.emit('socket_connection', ws)
+})
+
+eventEmitter.on('socket_connection', ctx => {
+    ctx.send('connection')
+    eventEmitter.emit('socket_binding', ctx)
+})
+
+eventEmitter.on('socket_binding', ctx => {
+    ctx.on('message', () => {
+        ctx.send('pong')
+    })
+})
+
+// todo broadcast
