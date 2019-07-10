2d084e2a1eb05ea08ac0a7aa723730934f798d92
lewis_dev li <lewis_dev@163.com>
2019-07-10 19:47:35 +0800

    update webrtc client sample

--- samples/client/js/main.js
+++ samples/client/js/main.js
@@ -8,15 +8,32 @@ media api:
     RTCDataChannel 交换消息
 */
 
-let video = document.getElementById('video')
-let snap = document.getElementById('snap')
 
-var context = canvas.getContext('2d');
+// view elements:
+
+const localVideo = document.getElementById('localVideo')
+const remoteVideo = document.getElementById('remoteVideo')
+const context = canvas.getContext('2d');
+
+const startButton = document.getElementById('startButton')
+const callButton = document.getElementById('callButton')
+const hangupButton = document.getElementById('hangupButton')
+const snap = document.getElementById('snap')
+
+callButton.disabled = true
+hangupButton.disabled = true
+
 
 // Trigger photo take
 snap.addEventListener("click", function () {
-    context.drawImage(video, 0, 0, 640, 480)
+    context.drawImage(localVideo, 0, 0, 640, 480)
 })
+
+
+
+
+
+
 
 let constraints = {
     audio: true,
@@ -26,20 +43,130 @@ let constraints = {
     }
 }
 
-function getLocalMediaSuccess(stream) {
-    window.stream = stream
-    console.log(window.stream)
-    video.srcObject = stream
+let localStream
+let remoteStream
 
-    video.play()
+let localPeerConnection
+let remotePeerConnection
+
+function getLocalMediaStream(stream) {
+    localStream = stream
+    localVideo.srcObject = localStream
 }
 
-function getLocalMediaError(error) {
-    console.log(error.name + ": " + error.message)
+function handleMediaStreamError(error) {
+    console.log(`getUserMedia error: ${error.toString()}`)
 }
 
-navigator
-    .mediaDevices
-    .getUserMedia(constraints)
-    .then(getLocalMediaSuccess)
-    .catch(getLocalMediaError)
+function getRemoteMediaStream(event) {
+    const stream = event.stream
+    remoteVideo.srcObject = stream
+}
+
+function getOtherPeer(peerConnection) {
+    return peerConnection === localPeerConnection ?
+        remotePeerConnection : localPeerConnection
+}
+
+
+function handleConnection(event) {
+    const peerConnection = event.target
+    const iceCandidate = event.candidate
+
+    if (iceCandidate) {
+        const newIceCandidate = new RTCIceCandidate(iceCandidate)
+        const otherPeer = getOtherPeer(peerConnection)
+
+        otherPeer.addIceCandidate(newIceCandidate)
+            .then(() => {
+                //
+            }).catch(error => {
+                //
+            })
+    }
+}
+
+function startAction() {
+
+    startButton.disabled = true
+    navigator
+        .mediaDevices
+        .getUserMedia(constraints)
+        .then(getLocalMediaStream)
+        .then(() => {
+            callButton.disabled = false
+        })
+        .catch(handleMediaStreamError)
+}
+
+// function setLocalDescription(peerConnection) {
+//     setDescriptionSucess(peerConnection, 'setLocalDescription')
+// }
+
+function createAnswer(description) {
+    remotePeerConnection.setLocalDescription(description)
+        .then(() => { }).catch(() => { })
+    localPeerConnection.setRemoteDescription(description)
+        .then(() => { }).catch(() => { })
+}
+
+function createdOffer(description) {
+
+    // localPeerConnection.setLocalDescription(description)
+    //     .then(() => {
+    //         setLocalDescriptionSuccess(localPeerConnection)
+    //     }).catch(() => { })
+
+    // remotePeerConnection.setLocalDescription(description)
+    //     .then(() => {
+    //         setRemoteDescriptionSuccess(remotePeerConnection)
+    //     }).catch(() => { })
+
+    remotePeerConnection.createAnswer()
+        .then(createdAnswer)
+        .catch(() => { })
+}
+
+function handleConnectionChange(event) {
+    
+}
+
+function callAction() {
+    callButton.disabled = true
+    hangupButton.disabled = false
+
+    const videoTracks = localStream.getVideoTracks()
+    const audioTracks = localStream.getAudioTracks()
+
+    const servers = null
+
+    localPeerConnection = new RTCPeerConnection(servers)
+
+    localPeerConnection.addEventListener('icecandidate', handleConnection)
+ localPeerConnection.addEventListener(
+    'iceconnectionstatechange', handleConnectionChange)
+
+
+    remotePeerConnection = new RTCPeerConnection(servers)
+
+    remotePeerConnection.addEventListener('icecandate', handleConnection)
+
+    localPeerConnection.addEventListener('addstream', getRemoteMediaStream)
+
+    localPeerConnection.addEventListener('addstream', getRemoteMediaStream)
+
+    localPeerConnection.addStream(localStream)
+
+    const offerOptions = {
+        offerToReceiveVideo: 1
+    }
+
+
+    localPeerConnection.createOffer(offerOptions).then(createdOffer).catch()
+
+}
+
+// binding event
+
+startButton.addEventListener('click', startAction)
+callButton.addEventListener('click', callAction)
