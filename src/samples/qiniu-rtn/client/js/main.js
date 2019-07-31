589e5afb3909bb2c1a4c34378f39e21e87ae27ea
lewis_dev li <lewis_dev@163.com>
2019-07-31 20:02:07 +0800

    update

--- src/samples/qiniu-rtn/client/js/main.js
+++ src/samples/qiniu-rtn/client/js/main.js
@@ -3,12 +3,42 @@
 // import { TrackModeSession, deviceManager, version } from "pili-rtc-web"
 import * as QNRTC from "pili-rtc-web"
 
-// todo global var [QNRTC] <- webpack
-window.QNRTC = QNRTC
-console.log("current version is", QNRTC.version)
+const vConsole = require("vconsole")
 
-window.ROOMTOKEN_1 = process.env.ROOMTOKEN_1
-console.log("room token", process.env.ROOMTOKEN_1)
+// const vconsole = new vConsole()
+
+// todo global var [QNRTC] <- webpack
+global.QNRTC = QNRTC
+
+global.ROOMTOKEN_1 = process.env.ROOMTOKEN_1
+global.ROOMTOKEN_2 = process.env.ROOMTOKEN_2
+global.ROOMTOKEN_3 = process.env.ROOMTOKEN_3
+global.ROOMTOKEN_4 = process.env.ROOMTOKEN_4
+
+global.getRoomToken = function (username, roomID) {
+    switch (username) {
+        case 'user1':
+            return ROOMTOKEN_1;
+        case 'user2':
+            return ROOMTOKEN_2;
+        case 'user3':
+            return ROOMTOKEN_3;
+        case 'user4':
+            return ROOMTOKEN_4;
+    }
+}
+
+global.joinRoomWithToken = async function (joinType, token) {
+    const method = `${joinType[0].toUpperCase()}${joinType.slice(1)}ModeSession`
+    // 初始化一个房间 Session 对象, 这里使用 Stream 模式
+    const myRoom = new QNRTC[method]
+    // 这里替换成刚刚生成的 RoomToken
+    await myRoom.joinRoomWithToken(token)
+
+    return myRoom
+}
+
+
 
 /*
 function genJwtToken() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
+const vConsolePlugin = require('vconsole-webpack-plugin')
 
 module.exports = {
   mode: 'production',
@@ -11,6 +12,10 @@ module.exports = {
   },
   plugins: [
     new Dotenv(),
+    new vConsolePlugin({
+      filter: [],  // 需要过滤的入口文件
+      enable: true // 发布代码前记得改回 false
+    }),
     new CopyPlugin([
       // { from: path.resolve(__dirname, 'src/samples/qiniu-rtn/client/live_server.js'), to: path.resolve(__dirname, 'dist/samples/qiniu-rtn/client/live_server.js') },
       { from: path.resolve(__dirname, 'src'), to: path.resolve(__dirname, 'dist'), ignore: ['*.js', '*.ml'], }
