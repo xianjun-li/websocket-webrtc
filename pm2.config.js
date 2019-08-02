f4601d311607a396fcf3d7b6cc164f986018a9f3
lewis_dev li <lewis_dev@163.com>
2019-08-02 11:19:35 +0800

    pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    apps: [
+        {
+            name: "sign_server",
+            script: "./src/samples/qiniu-rtn/server/sign.js",
+            watch: true,
+            env: {
+                "NODE_ENV": "development",
+            },
+            env_production: {
+                "NODE_ENV": "production"
+            }
+        },
+
+        {
+            name: "live_server",
+            script: "./src/samples/qiniu-rtn/client/live_server.js",
+            watch: true,
+            env: {
+                "NODE_ENV": "development",
+            },
+            env_production: {
+                "NODE_ENV": "production"
+            }
+        },
+
+             {
+            name: "watch_and_webpack",
+            script: "./scripts/compile.js",
+            watch: true,
+            env: {
+                "NODE_ENV": "development",
+            },
+            env_production: {
+                "NODE_ENV": "production"
+            }
+        },
+    ]
+} 
