d5c203a7617635a60cb9951398dd198766eec85f
lewis_dev li <lewis_dev@163.com>
2019-08-14 10:21:15 +0800

    重构:名称

--- src/samples/qiniu-rtn/client/js/main.js
+++ src/samples/qiniu-rtn/client/js/main.js
@@ -39,10 +39,9 @@ global.detectWebRTC = function () {
 }
 
 global.joinRoomWithToken = async function (joinType, token) {
-    const method = `${joinType[0].toUpperCase()}${joinType.slice(1)}ModeSession`
-    // 初始化一个房间 Session 对象, 这里使用 Stream 模式
-    const myRoom = new QNRTC[method]
-    // 这里替换成刚刚生成的 RoomToken
+    const sessionType = `${joinType[0].toUpperCase()}${joinType.slice(1)}ModeSession`
+    // 初始化一个房间 Session 对象
+    const myRoom = new QNRTC[sessionType]
     try {
         await myRoom.joinRoomWithToken(token)
     } catch (error) {
@@ -52,49 +51,3 @@ global.joinRoomWithToken = async function (joinType, token) {
 
     return myRoom
 }
-
-/*
-function genJwtToken() {
-    const token = jsonwebtoken.sign({ foo: 'bar' }, 'ssssss')
-    return token
-}
-
-// @todo 自定义子协议
-const ws_protocols = []
-
-const ws = new WebSocket(ws_url, ws_protocols)
-console.log(ws)
-
-//events: open, message, error, close
-
-ws.addEventListener('open', event => {
-
-    // 授权
-    const auth_content = {
-        action: 'auth',
-        type: 'jwt',
-        token: genJwtToken()
-    }
-    ws.send(JSON.stringify(auth_content))
-})
-
-ws.addEventListener('message', event => {
-    console.log(event.data)
-})
-
-ws.addEventListener('close', () => {
-    console.log('websocket close')
-})
-
-let n = 0
-let timer = setInterval(() => {
-    console.log('interval:' + n)
-    if (n === 20) {
-        console.log('interval end')
-        clearInterval(timer)
-    } else {
-        ws.send('ping')
-        n++
-    }
-}, 500)
-*/
